ff2a5064dd2a179870d3d1848e0b248736c31528
Ondrej Brinkel <ondrej@studiobosco.de>
2021-02-05 12:56:11 +0100

    adds pagination to unsplash search

--- src/drupal/web/modules/custom/le_webbuilder/assets/js/unsplash_media_form.js
+++ src/drupal/web/modules/custom/le_webbuilder/assets/js/unsplash_media_form.js
@@ -19,59 +19,41 @@ if (typeof 'debounce' !== 'function') {
   };
 }
 
-function initUnsplashPreview() {
-  const previewTarget = document.querySelector('.unsplash-preview');
-  const urlSource = document.querySelector(previewTarget.getAttribute('data-url-source'));
-  const altSource = document.querySelector(previewTarget.getAttribute('data-alt-source'));
-  const url = urlSource.value.replace('.jpg', '&w=400');
-  const alt = altSource.value;
-  previewTarget.innerHTML = `
-    <img src="${url}" alt="${alt}">
-  `;
-}
+(() => {
+  let unsplashClient;
 
-let unsplashClient;
-
-const handleUnsplashSearchInput = debounce((input) => {
-  const query = (input.value || '').trim();
-  const page = parseInt(input.getAttribute('page') || 1, 10);
-  const resultsTarget = document.querySelector(input.getAttribute('data-results-target'));
-  const attributionTarget = document.querySelector(input.getAttribute('data-attribution-target'));
-  const previewTarget = document.querySelector(input.getAttribute('data-preview-target'));
-  const urlTarget = document.querySelector(input.getAttribute('data-url-target'));
-  const altTarget = document.querySelector(input.getAttribute('data-alt-target'));
-  const titleTarget = document.querySelector(input.getAttribute('data-title-target'));
-
-  // only allow minimum length
-  if (query.length < 3) {
-    resultsTarget.innerHTML = '';
-    return;
-  }
-
-  // create client, if not yet done so
-  if (!unsplashClient) {
-    const apiUrl = input.getAttribute('data-api-url');
-    unsplashClient = new Unsplash.default({
+  function createUnsplashClient(apiUrl) {
+    return new Unsplash.default({
       apiUrl: apiUrl
     });
   }
 
-  resultsTarget.innerHTML = '<p class="unsplash-results__info">Suche Bilder ...</p>';
+  function renderPreview() {
+    const previewTarget = document.querySelector('.unsplash-preview');
+    const urlSource = document.querySelector(previewTarget.getAttribute('data-url-source'));
+    const altSource = document.querySelector(previewTarget.getAttribute('data-alt-source'));
+    const url = urlSource.value.replace('.jpg', '&w=400');
+    const alt = altSource.value;
+    previewTarget.innerHTML = `
+      <img src="${url}" alt="${alt}">
+    `;
+  }
 
-  unsplashClient.search.photos(query, page)
-  .then(res => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      throw new Error('Invalid Response');
-    }
-  })
-  .then(data => {
-    if (!data.results.length) {
-      resultsTarget.innerHTML = '<p class="unsplash-results__info">Keine Bilder gefunden. Bitte versuchen Sie einen anderen Suchbegriff.</p>';
-      return;
-    }
-    const html = data.results.reduce((html, result, index) => {
+  function renderClear(target) {
+    target.innerHTML = '';
+  }
+
+  function renderSearching(target) {
+    target.innerHTML = '<p class="unsplash-results__info">Suche Bilder ...</p>';
+  }
+
+  function renderNoResults(target) {
+    target.innerHTML = '<p class="unsplash-results__info">Keine Bilder gefunden. Bitte versuchen Sie einen anderen Suchbegriff.</p>';
+  }
+
+  function renderResults(target, data, opts = {}) {
+    let html = '<div class="unsplash-results__items">';
+    html += data.results.reduce((html, result, index) => {
       return html + `
         <figure class="unsplash-results__item" data-role="wrapper" data-index="${index}">
           <a class="unsplash-results__item-image" target="_blank" href="${result.links.download}">
@@ -94,41 +76,136 @@ const handleUnsplashSearchInput = debounce((input) => {
         </figure>
       `;
     }, '');
-    resultsTarget.innerHTML = html;
-    Array.from(resultsTarget.querySelectorAll('*[data-role="wrapper"]')).forEach((el, index) => {
+    html += '</div>';
+    if (data.total_pages > 0) {
+      html += '<div class="unsplash-results__pagination">';
+      if (opts.page > 1) {
+        html += `
+        <button data-role="prev-page" class="button button--default unsplash-results__prev-page">
+          Zurück
+        </button>
+        `;
+      }
+      html += `
+      <div class="unsplash-results__current-page">
+        ${opts.page} / ${data.total_pages}
+      </div>
+      `;
+      if (opts.page < data.total_pages) {
+        html += `
+        <button data-role="next-page" class="button button--default unsplash-results__next-page">
+          Weitere anzeigen
+        </button>
+        `;
+      }
+      html += '</div>';
+    }
+
+    target.innerHTML = html;
+
+    // attach listeners
+    Array.from(target.querySelectorAll('*[data-role="wrapper"]')).forEach((el, index) => {
       const result = data.results[index];
-      el.querySelector('*[data-role="add"]').addEventListener('click', (event) => {
-        event.preventDefault();
-        urlTarget.value = result.urls.raw + '.jpg';
-        altTarget.value = result.alt_description || result.description || '';
-        titleTarget.value = result.description || result.alt_description || '';
-        attributionTarget.value = `
-        <p>${result.description || result.alt_description || ''}</p>
-        <p>
-          by <a target="_blank" href="${result.user.links.html}">
-            ${result.user.name} (${result.user.links.html})
-          </a>
-          on Unsplash
-        </p>
-        `;
-
-        // trigger a download, as required by the unsplash API usage terms
-        unsplashClient.photos.trackDownload(result);
-
-        // clear search input and results
-        input.value = '';
-        resultsTarget.innerHTML = '';
-        previewTarget.innerHTML = `
-          <img src="${result.urls.small}" alt="${result.alt_description || result.description || ''}">
-        `;
-        return false;
-      });
+      el.querySelector('*[data-role="add"]').addEventListener('click', (event) => opts.onResultAdd(event, result));
     });
-  })
-  .catch(err => {
-    console.warn(err);
-  });
-}, 1000);
 
+    if (data.total_pages > 1) {
+      if (opts.page > 1) {
+        target.querySelector('*[data-role="prev-page"]').addEventListener('click', (event) => opts.onPrevPage(event, opts.page));
+      }
+      if (opts.page < data.total_pages) {
+        target.querySelector('*[data-role="next-page"]').addEventListener('click', (event) => opts.onNextPage(event, opts.page));
+      }
+    }
+  }
 
-initUnsplashPreview();
+  function getAttribution(result) {
+    return `
+    <p>${result.description || result.alt_description || ''}</p>
+    <p>
+      by <a target="_blank" href="${result.user.links.html}">
+        ${result.user.name} (${result.user.links.html})
+      </a>
+      on Unsplash
+    </p>
+    `;
+  }
+
+  function handleUnsplashSearchInput(input, page = 1) {
+    const query = (input.value || '').trim();
+    const resultsTarget = document.querySelector(input.getAttribute('data-results-target'));
+    const attributionTarget = document.querySelector(input.getAttribute('data-attribution-target'));
+    const urlTarget = document.querySelector(input.getAttribute('data-url-target'));
+    const altTarget = document.querySelector(input.getAttribute('data-alt-target'));
+    const titleTarget = document.querySelector(input.getAttribute('data-title-target'));
+
+    // only allow minimum length
+    if (query.length < 3) {
+      renderClear(resultsTarget);
+      return;
+    }
+
+    // create client, if not yet done so
+    if (!unsplashClient) {
+      unsplashClient = createUnsplashClient(
+        input.getAttribute('data-api-url')
+      );
+    }
+
+    if (page === 1) {
+      renderSearching(resultsTarget);
+    }
+
+    unsplashClient.search.photos(query, page)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error('Invalid Response');
+      }
+    })
+    .then(data => {
+      if (!data.results.length) {
+        renderNoResults(resultsTarget);
+        return;
+      }
+
+      renderResults(resultsTarget, data, {
+        page: page,
+        onResultAdd: (event, result) => {
+          event.preventDefault();
+          urlTarget.value = result.urls.raw + '.jpg';
+          altTarget.value = result.alt_description || result.description || '';
+          titleTarget.value = result.description || result.alt_description || '';
+          attributionTarget.value = getAttribution(result);
+
+          // trigger a download, as required by the unsplash API usage terms
+          unsplashClient.photos.trackDownload(result);
+
+          // clear search input and results
+          input.value = '';
+          renderClear(resultsTarget);
+          renderPreview();
+          return false;
+        },
+        onNextPage: (event, page) => {
+          event.preventDefault();
+          handleUnsplashSearchInput(input, page + 1);
+          return false;
+        },
+        onPrevPage: (event, page) => {
+          event.preventDefault();
+          handleUnsplashSearchInput(input, page - 1);
+          return false;
+        }
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+  window.handleUnsplashSearchInput = debounce(handleUnsplashSearchInput, 2000);
+
+  renderPreview();
+})();
