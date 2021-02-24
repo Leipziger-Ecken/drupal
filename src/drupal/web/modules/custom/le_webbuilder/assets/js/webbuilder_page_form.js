4b10048bf6fabfb332b9d17e7e6034de8a383365
Ondrej Brinkel <ondrej@studiobosco.de>
2021-02-24 21:56:52 +0100

    adjusts webbuilder page form

--- src/drupal/web/modules/custom/le_webbuilder/assets/js/webbuilder_page_form.js
+++ src/drupal/web/modules/custom/le_webbuilder/assets/js/webbuilder_page_form.js
@@ -1,13 +1,25 @@
 (() => {
   const titleInput = document.getElementById('edit-title-0-value');
   const slugInput = document.getElementById('edit-field-slug-0-value');
+  let slugManuallyUpdated = !!slugInput.value.trim();
 
-  function updateSlug() {
-    slugInput.value = slugify(titleInput.value);
+  function handleTitleInput() {
+    if (!slugManuallyUpdated) {
+      slugInput.value = slugify(titleInput.value);
+    }
   }
 
-  titleInput.addEventListener('input', updateSlug);
+  function handleSlugInput() {
+    if (!slugInput.value.trim()) {
+      slugManuallyUpdated = false;
+    } else {
+      slugInput.value = slugify(slugInput.value);
+    }
+  }
+
+  titleInput.addEventListener('input', handleTitleInput);
+  slugInput.addEventListener('input', handleSlugInput);
 
   // if slug was not set yet, update it
-  if (titleInput.value.trim() && !slugInput.value.trim()) updateSlug();
+  if (!slugManuallyUpdated) updateSlug();
 })();
