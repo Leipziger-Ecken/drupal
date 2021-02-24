21bf9a2b06a61ab007d23ff4d2004e5895fe2481
Ondrej Brinkel <ondrej@studiobosco.de>
2021-02-24 20:31:16 +0100

    adjusts user profile pages

--- src/drupal/web/modules/custom/le_admin/src/Breadcrumb/BreadcrumbBuilder.php
+++ src/drupal/web/modules/custom/le_admin/src/Breadcrumb/BreadcrumbBuilder.php
@@ -15,9 +15,13 @@ class BreadcrumbBuilder implements BreadcrumbBuilderInterface{
     $route_name = $route_match->getRouteName();
     $parameters = $route_match->getParameters()->all();
     $node_type = isset($parameters['node']) ? $parameters['node']->getType() : null;
+    // dd($route_name);
 
     return (
       in_array($route_name, [
+        'entity.user.canonical',
+        'entity.user.edit_form',
+        'entity.user.cancel_form',
         'le_admin.user_dashboard',
       ]) ||
       in_array($node_type, [
@@ -40,10 +44,41 @@ class BreadcrumbBuilder implements BreadcrumbBuilderInterface{
     $parameters = $route_match->getParameters()->all();
     $route_name = $route_match->getRouteName();
     $node_type = isset($parameters['node']) ? $parameters['node']->getType() : null;
-
+    
     $breadcrumb->addLink(
       Link::createFromRoute(t('Übersicht'), 'le_admin.user_dashboard')
     );
+
+    if (in_array($route_name, [
+      'entity.user.canonical',
+      'entity.user.edit_form',
+      'entity.user.cancel_form',
+    ])) {
+      $breadcrumb->addLink(
+        Link::createFromRoute(
+          t('Mein Konto'),
+          'entity.user.canonical', ['user' => \Drupal::currentUser()->id()]
+        )
+      );
+    }
+
+    if ($route_name === 'entity.user.edit_form') {
+      $breadcrumb->addLink(
+        Link::createFromRoute(
+          t('Edit'),
+          'entity.user.edit_form', ['user' => \Drupal::currentUser()->id()]
+        )
+      );
+    }
+
+    if ($route_name === 'entity.user.cancel_form') {
+      $breadcrumb->addLink(
+        Link::createFromRoute(
+          t('Cancel account'),
+          'entity.user.cancel_form', ['user' => \Drupal::currentUser()->id()]
+        )
+      );
+    }
 
     if ($route_name === 'le_admin.user_akteur_contents') {
       $node = $parameters['node'];
